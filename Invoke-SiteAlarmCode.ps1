fd3e2f6248bb87cd221c1da197fa680a7bb89d42
ChrisMagnuson <hourglasssand@gmail.com>
2017-06-08 16:59:31 -0400

    Old tornado siren Invoke-SiteAlarmCode

---
+++ Invoke-SiteAlarmCode.ps1
@@ -0,0 +1,50 @@
+﻿function Test-SiteFunctioning {
+    $url="http://www.tervis.com"
+    $wc = new-object net.webclient
+    $html = $wc.DownloadString($url) 
+    return $html.contains("Join the conversation!")
+}
+
+function Invoke-SiteAlarmCode {
+    $ErrCount = 0
+    
+    While ($True -eq $True) {
+        Try {
+            $isSiteFunctioning = Test-SiteFunctioning
+            
+            Write-host "The site is functioning $isSiteFunctioning"
+            
+            if ( $isSiteFunctioning -eq $False) {
+                write-host "Problem with website $ErrCount"
+                $ErrCount++
+                
+                if ($ErrCount -gt 2) {
+                    write-host "Site has experienced $ErrCount consecutive errors"
+                    (new-object Media.SoundPlayer "C:\tornado.wav").play();
+                    start-sleep -s 15
+                    $ErrCount = 0                    
+                }
+                
+                start-sleep -s 5                
+            }
+            else {
+                start-sleep -s 5
+            }
+        }
+        catch {
+            write-host "exception found"
+            $_
+            write-host "Problem with website $ErrCount"
+            $ErrCount++
+            
+            if ($ErrCount -gt 2) {
+               write-host "Site has experienced $ErrCount consecutive errors"
+               (new-object Media.SoundPlayer "C:\tornado.wav").play();
+               start-sleep -s 15
+               $ErrCount = 0                      
+            }
+            
+            start-sleep -s 5
+        }
+    }
+}
